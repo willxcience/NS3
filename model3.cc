2750748168df843d9d489ed3a6a4c6f866567d30
will <wilwu.ucdavis.edu>
2017-08-04 12:44:05 -0700

    0804

--- model3.cc
+++ model3.cc
@@ -19,7 +19,6 @@
  *         Nicola Baldo <nbaldo@cttc.es>
  */
 
-
 #include "ns3/lte-helper.h"
 #include "ns3/epc-helper.h"
 #include "ns3/core-module.h"
@@ -35,19 +34,21 @@
 #include <iomanip>
 #include <string>
 
+#define D 500
+#define OFFSET 200
+
 using namespace ns3;
 using namespace std;
 
-
 class UserEquipment
 {
-public:
+  public:
     uint32_t imsi;   //like an index
     uint32_t cellid; //which cellid
     uint32_t nodeid;
     double sinr; //SINR averaged among RBs
     double rsrp;
-    
+
     void init(uint32_t imsi, uint32_t cellid, uint32_t nodeid)
     {
         this->imsi = imsi;
@@ -60,50 +61,46 @@ public:
 
 UserEquipment UEs[2];
 
-
 uint16_t getNodeNumber(string context)
 {
     uint32_t pos = context.find('/', 10);
     return atoi(context.substr(10, pos - 10).c_str());
 }
 
-
-void handler(NetDeviceContainer enbLteDevs, NetDeviceContainer ueLteDevs, NetDeviceContainer ueLteDevs2)
+void handler(NetDeviceContainer enbLteDevs, NetDeviceContainer ueLteDevs)
 {
     //Ptr<MobilityModel> mobility = ueLteDevs.Get(1)->GetNode()->GetObject<MobilityModel>();
     //Vector3D pos = mobility->GetPosition();
-    
+
     //from 0 to 3
     Ptr<LteEnbPhy> enbPhy = enbLteDevs.Get(0)->GetObject<LteEnbNetDevice>()->GetPhy();
     double currTxPower = enbPhy->GetTxPower();
 
-    
     cout << enbLteDevs.Get(0)->GetObject<LteEnbNetDevice>()->GetPhy()->GetTxPower() << "db" << endl;
     cout << ueLteDevs.Get(0)->GetObject<LteUeNetDevice>()->GetPhy()->GetTxPower() << "dbm" << endl;
-    
-    enbPhy->SetTxPower(190.0);
-    
 
-    
+    enbPhy->SetTxPower(35.0);
+
     cout << "IMSI: " << UEs[0].imsi << " SINR: " << UEs[0].sinr << " RSRP: " << UEs[0].rsrp << endl;
     cout << "IMSI: " << UEs[1].imsi << " SINR: " << UEs[1].sinr << " RSRP: " << UEs[1].rsrp << endl;
-    
+    cout << "IMSI: " << UEs[2].imsi << " SINR: " << UEs[2].sinr << " RSRP: " << UEs[2].rsrp << endl;
+    cout << "IMSI: " << UEs[3].imsi << " SINR: " << UEs[3].sinr << " RSRP: " << UEs[3].rsrp << endl;
+
     /* */
-    
+
     //schedule next control
-    Simulator::Schedule(Seconds(1.0), &handler, enbLteDevs, ueLteDevs, ueLteDevs2);
+    Simulator::Schedule(Seconds(1.0), &handler, enbLteDevs, ueLteDevs);
     cout << endl;
 }
-
 
 //Trace Sink
 static void ReportCurrentCellRsrpSinr(string context, uint16_t cellId, uint16_t rnti, double rsrp, double sinr)
 {
-    int id = getNodeNumber(context) - 2;
-    
+    int id = getNodeNumber(context) - 4;
+
     //cout << "CellId " << cellId << " SINR: " << sinr << endl;
     //record data
-    
+
     //cout << id << endl;
     UEs[id].sinr = sinr;
     UEs[id].rsrp = rsrp;
@@ -112,136 +109,108 @@ static void ReportCurrentCellRsrpSinr(string context, uint16_t cellId, uint16_t 
 static void NotifyConnectionEstablishedUe(string context, uint64_t imsi, uint16_t cellid, uint16_t rnti)
 {
     cout << std::setprecision(8) << "Time " << Simulator::Now().GetSeconds()
-    << " UE IMSI " << imsi
-    << ": connected to CellId " << cellid
-    << " with Node ID " << getNodeNumber(context)
-    << endl;
-    
+         << " UE IMSI " << imsi
+         << ": connected to CellId " << cellid
+         << " with Node ID " << getNodeNumber(context)
+         << endl;
+
     UEs[imsi - 1].init(imsi, cellid, getNodeNumber(context));
     cout << "UE:" << imsi << " initialized!" << endl;
 }
 
-
-
-
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  double enbDist = 100.0;
-  double radius = 50.0;
-  uint32_t numUes = 1;
-  double simTime = 10.0;
+    double enbDist = 100.0;
+    double radius = 50.0;
+    uint32_t numUes = 1;
+    double simTime = 10.0;
 
-  CommandLine cmd;
-  cmd.AddValue ("enbDist", "distance between the two eNBs", enbDist);
-  cmd.AddValue ("radius", "the radius of the disc where UEs are placed around an eNB", radius);
-  cmd.AddValue ("numUes", "how many UEs are attached to each eNB", numUes);
-  cmd.AddValue ("simTime", "Total duration of the simulation (in seconds)", simTime);
-  cmd.Parse (argc, argv);
+    CommandLine cmd;
+    cmd.AddValue("enbDist", "distance between the two eNBs", enbDist);
+    cmd.AddValue("radius", "the radius of the disc where UEs are placed around an eNB", radius);
+    cmd.AddValue("numUes", "how many UEs are attached to each eNB", numUes);
+    cmd.AddValue("simTime", "Total duration of the simulation (in seconds)", simTime);
+    cmd.Parse(argc, argv);
 
-  ConfigStore inputConfig;
-  inputConfig.ConfigureDefaults ();
+    ConfigStore inputConfig;
+    inputConfig.ConfigureDefaults();
 
-  // parse again so you can override default values from the command line
-  cmd.Parse (argc, argv);
+    // parse again so you can override default values from the command line
+    cmd.Parse(argc, argv);
 
-  // determine the string tag that identifies this simulation run
-  // this tag is then appended to all filenames
+    // determine the string tag that identifies this simulation run
+    // this tag is then appended to all filenames
 
-  IntegerValue runValue;
-  GlobalValue::GetValueByName ("RngRun", runValue);
-      Config::SetDefault("ns3::LteSpectrumPhy::CtrlErrorModelEnabled", BooleanValue(false));
+    IntegerValue runValue;
+    GlobalValue::GetValueByName("RngRun", runValue);
+    Config::SetDefault("ns3::LteSpectrumPhy::CtrlErrorModelEnabled", BooleanValue(false));
 
-      Config::SetDefault("ns3::LteHelper::UseIdealRrc", BooleanValue(true));
-      Config::SetDefault("ns3::LteEnbPhy::TxPower", DoubleValue(200.0));
-    Config::SetDefault("ns3::LteUePhy::EnableUplinkPowerControl", BooleanValue(false));
+    Config::SetDefault("ns3::LteEnbPhy::TxPower", DoubleValue(30.0));
+    //Config::SetDefault("ns3::LteUePhy::EnableUplinkPowerControl", BooleanValue(false));
 
+    Ptr<LteHelper> lteHelper = CreateObject<LteHelper>();
 
-  std::ostringstream tag;
-  tag  << "_enbDist" << std::setw (3) << std::setfill ('0') << std::fixed << std::setprecision (0) << enbDist
-       << "_radius"  << std::setw (3) << std::setfill ('0') << std::fixed << std::setprecision (0) << radius
-       << "_numUes"  << std::setw (3) << std::setfill ('0')  << numUes
-       << "_rngRun"  << std::setw (3) << std::setfill ('0')  << runValue.Get () ;
+    lteHelper->SetAttribute("PathlossModel", StringValue("ns3::FriisSpectrumPropagationLossModel"));
 
-  Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
-  
-  lteHelper->SetAttribute ("PathlossModel", StringValue ("ns3::FriisPropagationLossModel"));
+    /***********Mobility***********/
+    NodeContainer ueNodes;
+    NodeContainer enbNodes;
+    enbNodes.Create(4);
+    ueNodes.Create(4); //4 user equipments
 
-  // Create Nodes: eNodeB and UE
-  NodeContainer enbNodes;
-  NodeContainer ueNodes1, ueNodes2;
-  enbNodes.Create (2);
-  ueNodes1.Create (numUes);
-  ueNodes2.Create (numUes);
+    // Install Mobility Model in eNB
+    Ptr<ListPositionAllocator> enbPositions = CreateObject<ListPositionAllocator>();
+    //Quad 1
+    enbPositions->Add(Vector(D / 2, D / 2, 10));
+    enbPositions->Add(Vector(D * 1.5, D / 2, 10));
+    enbPositions->Add(Vector(D / 2, D * 1.5, 10));
+    enbPositions->Add(Vector(1.5 * D, D * 1.5, 10));
+    MobilityHelper enbMobility;
+    enbMobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    enbMobility.SetPositionAllocator(enbPositions);
+    enbMobility.Install(enbNodes);
 
-  // Position of eNBs
-  Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
-  positionAlloc->Add (Vector (0.0, 0.0, 0.0));
-  positionAlloc->Add (Vector (enbDist, 0.0, 0.0));
-  MobilityHelper enbMobility;
-  enbMobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-  enbMobility.SetPositionAllocator (positionAlloc);
-  enbMobility.Install (enbNodes);
+    //Set the user equipments positions
+    MobilityHelper ueMobility;
+    Ptr<ListPositionAllocator> uePositions = CreateObject<ListPositionAllocator>();
+    uePositions->Add(Vector(D - OFFSET, D - OFFSET, 10));
+    uePositions->Add(Vector(D + OFFSET, D - OFFSET, 10));
+    uePositions->Add(Vector(D - OFFSET, D + OFFSET, 10));
+    uePositions->Add(Vector(D + OFFSET, D + OFFSET, 10));
+    ueMobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    ueMobility.SetPositionAllocator(uePositions);
+    ueMobility.Install(ueNodes);
 
-  // Position of UEs attached to eNB 1
-  MobilityHelper ue1mobility;
-  ue1mobility.SetPositionAllocator ("ns3::UniformDiscPositionAllocator",
-                                    "X", DoubleValue (0.0),
-                                    "Y", DoubleValue (0.0),
-                                    "rho", DoubleValue (radius));
-  ue1mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-  ue1mobility.Install (ueNodes1);
+    /***Next Part***/
+    // Install LTE Devices to the nodes
+    NetDeviceContainer enbLteDevs = lteHelper->InstallEnbDevice(enbNodes);
+    NetDeviceContainer ueLteDevs = lteHelper->InstallUeDevice(ueNodes);
 
-  // Position of UEs attached to eNB 2
-  MobilityHelper ue2mobility;
-  ue2mobility.SetPositionAllocator ("ns3::UniformDiscPositionAllocator",
-                                    "X", DoubleValue (enbDist),
-                                    "Y", DoubleValue (0.0),
-                                    "rho", DoubleValue (radius));
-  ue2mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-  ue2mobility.Install (ueNodes2);
+    // Attach UEs to a eNB
+    lteHelper->Attach(ueLteDevs.Get(0), enbLteDevs.Get(0));
+    lteHelper->Attach(ueLteDevs.Get(1), enbLteDevs.Get(1));
+    lteHelper->Attach(ueLteDevs.Get(2), enbLteDevs.Get(2));
+    lteHelper->Attach(ueLteDevs.Get(3), enbLteDevs.Get(3));
 
+    // Activate a data radio bearer each UE
+    enum EpsBearer::Qci q = EpsBearer::GBR_CONV_VOICE;
+    EpsBearer bearer(q);
+    lteHelper->ActivateDataRadioBearer(ueLteDevs.Get(0), bearer);
+    lteHelper->ActivateDataRadioBearer(ueLteDevs.Get(1), bearer);
+    lteHelper->ActivateDataRadioBearer(ueLteDevs.Get(2), bearer);
+    lteHelper->ActivateDataRadioBearer(ueLteDevs.Get(3), bearer);
 
-
-  // Create Devices and install them in the Nodes (eNB and UE)
-  NetDeviceContainer enbDevs;
-  NetDeviceContainer ueDevs1;
-  NetDeviceContainer ueDevs2;
-  enbDevs = lteHelper->InstallEnbDevice (enbNodes);
-  ueDevs1 = lteHelper->InstallUeDevice (ueNodes1);
-  ueDevs2 = lteHelper->InstallUeDevice (ueNodes2);
-
-  // Attach UEs to a eNB
-  lteHelper->Attach (ueDevs1, enbDevs.Get (0));
-  lteHelper->Attach (ueDevs2, enbDevs.Get (1));
-
-  // Activate a data radio bearer each UE
-  enum EpsBearer::Qci q = EpsBearer::GBR_CONV_VOICE;
-  EpsBearer bearer (q);
-  lteHelper->ActivateDataRadioBearer (ueDevs1, bearer);
-  lteHelper->ActivateDataRadioBearer (ueDevs2, bearer);
-
-    
     //Custom Trace Sinks for measuring Handover
     Config::Connect("/NodeList/*/DeviceList/*/LteUeRrc/ConnectionEstablished",
                     MakeCallback(&NotifyConnectionEstablishedUe));
     Config::Connect("/NodeList/*/DeviceList/*/LteUePhy/ReportCurrentCellRsrpSinr",
                     MakeCallback(&ReportCurrentCellRsrpSinr));
-    
-    
-  Simulator::Schedule(Seconds(1.0), &handler, enbDevs, ueDevs1, ueDevs2);
-    
-  Simulator::Stop (Seconds (simTime));
 
-  // Insert RLC Performance Calculator
-  std::string dlOutFname = "DlRlcStats";
-  dlOutFname.append (tag.str ());
-  std::string ulOutFname = "UlRlcStats";
-  ulOutFname.append (tag.str ());
+    Simulator::Schedule(Seconds(1.0), &handler, enbLteDevs, ueLteDevs);
 
-  lteHelper->EnableMacTraces ();
-  lteHelper->EnableRlcTraces ();
+    Simulator::Stop(Seconds(simTime));
 
-  Simulator::Run ();
-  Simulator::Destroy ();
-  return 0;
+    Simulator::Run();
+    Simulator::Destroy();
+    return 0;
 }
